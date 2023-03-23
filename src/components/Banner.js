759e7ccefbaca23f95fbb4c58a54690368f71fa6
Sarah Bocognano <sarah.bocognano@gmail.com>
2023-03-23 15:16:02 +0100

    Add Skills Component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
-import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from '../assets/img/header-img.svg';
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -9,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ['Front-End Developer', 'FullStack Developper', 'BackEnd Developper'];
+  const toRotate = [ "FullStack Developer", "FullStack Software Engineer", "UI/UX Designer" ];
   const period = 2000;
 
   useEffect(() => {
@@ -17,18 +19,18 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker)};
+    return () => { clearInterval(ticker) };
   }, [text])
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.lenght + 1);
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
-    if(isDeleting) {
-      setDelta(prevDelta => prevDelta / 2)
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
@@ -45,21 +47,31 @@ export const Banner = () => {
     }
   }
 
-    return (
-        <section className="banner" id="home">
-            <Container>
-              <Row className="align-items-center">
-                <Col xs={12} md={6} xl={7}>
-                  <span className="tagline">Welcome to my Portfolio</span>
-                  <h1>{`Hi I'm Reila `}<span className="wrap">{text}</span></h1>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25} /></button>
-                </Col>
-                <Col xs={12} md={6} xl={5}>
-                  <img src={headerImg} alt="HeaderImg" />
-                </Col>
-              </Row>
-            </Container>
-        </section>
-    )
-};
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Reila`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "FullStack Developer", "FullStack Software Engineer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
